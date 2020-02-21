243549660a8bd2925c65c5ddb9083dfca62920ea
jjsanderson <jonathan.sanderson@northumbria.ac.uk>
2020-02-21 13:10:21 +0000

    First working actual block. Clone of servo_write.

--- blockly/blocks/arduino/connect_blocks.js
+++ blockly/blocks/arduino/connect_blocks.js
@@ -1,8 +1,8 @@
 /**
-* @license Copyright (c) 2020 Northumbria University, Newcastle, UK.
-conNecT is license under FIXME:
-*
-*/
+ * @license Copyright (c) 2020 Northumbria University, Newcastle, UK.
+ * conNecT is licensed under FIXME:
+ *
+ */
 
 /** 
  * @fileoverview Blocks for the conNecT library
@@ -20,30 +20,79 @@ Blockly.Blocks.connect_conNecT.HUE = "#de6739";
 
 //=== conNecT test functions ===========================================
 
-Blockly.Blocks['connect_test_block'] = {
+Blockly.Blocks["connect_test_block"] = {
+  /**
+   * Block to test that I understand where all the moving parts go.
+   */
+  init: function() {
+    this.appendDummyInput().appendField("cabbages");
+    this.appendValueInput("RED")
+      .setCheck(null)
+      .appendField("then")
+      .appendField(new Blockly.FieldTextInput("default"), "NAME")
+      .appendField(new Blockly.FieldAngle(90), "NAME")
+      .appendField(
+        new Blockly.FieldDropdown([
+          ["red", "RD"],
+          ["green", "G"],
+          ["blue", "B"]
+        ]),
+        "colourE"
+      )
+      .appendField(new Blockly.FieldCheckbox("TRUE"), "NAME")
+      .appendField(new Blockly.FieldColour("#ff0000"), "NAME")
+      .appendField(new Blockly.FieldVariable("item"), "NAME");
+    this.appendStatementInput("NAME")
+      .setCheck(null)
+      .appendField("if");
+    this.setInputsInline(false);
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(160);
+    this.setTooltip("FIXME");
+    this.setHelpUrl("FIXME");
+  }
+};
+
+  Blockly.Blocks["connect_smooth_servo_block"] = {
     /**
-     * Block to test that I understand where all the moving parts go.
+     * Block implementing ServoEasing servo movement, with speed selector.
+     * Based heavily on default servo_write block
+     * @this Blockly.Block
      */
     init: function() {
+      this.setHelpUrl("https://FIXME:");
+      this.setColour(Blockly.Blocks.servo.HUE);
       this.appendDummyInput()
-          .appendField("cabbages");
-      this.appendValueInput("RED")
-          .setCheck(null)
-          .appendField("then")
-          .appendField(new Blockly.FieldTextInput("default"), "NAME")
-          .appendField(new Blockly.FieldAngle(90), "NAME")
-          .appendField(new Blockly.FieldDropdown([["red","RD"], ["green","G"], ["blue","B"]]), "colourE")
-          .appendField(new Blockly.FieldCheckbox("TRUE"), "NAME")
-          .appendField(new Blockly.FieldColour("#ff0000"), "NAME")
-          .appendField(new Blockly.FieldVariable("item"), "NAME");
-      this.appendStatementInput("NAME")
-          .setCheck(null)
-          .appendField("if");
+        .appendField('Move SERVO on Pin')
+        .appendField(
+          new Blockly.FieldDropdown(Blockly.Arduino.Boards.selected.pwmPins),
+          "SERVO_PIN"
+        );
       this.setInputsInline(false);
+      this.appendValueInput('SERVO_ANGLE')
+        .setCheck(Blockly.Types.NUMBER.checkList)
+        .appendField('to angle');
+      this.appendValueInput('SWEEP_TIME')
+        .setCheck(Blockly.Types.NUMBER.checkList)
+        .appendField('in');
+      this.appendDummyInput().appendField('secs');
+      this.setInputsInline(true);
       this.setPreviousStatement(true, null);
       this.setNextStatement(true, null);
-      this.setColour(160);
-   this.setTooltip("FIXME");
-   this.setHelpUrl("FIXME");
+      this.setTooltip("Test tooltip");
+    },
+    /**
+     * Updates the content of the pin-related fields
+     * @this Blockly.Block
+     */
+    updateFields: function() {
+      Blockly.Arduino.Boards.refreshBlockFieldDropdown(
+        this,
+        "SERVO_PIN",
+        "pwmPins"
+      );
     }
   };
+
+  
--- blockly/generators/arduino/connect_code.js
+++ blockly/generators/arduino/connect_code.js
@@ -29,3 +29,35 @@ Blockly.Arduino['connect_test_block'] = function(block) {
     var code = 'Kniwwelino.getMAC();\n';
     return code;
 };
+
+/**
+   * Connect smooth servo movement.
+   * Based on default servo block
+   * Arduino code: #include <Servo.h>
+   *               Servo myServoX;
+   *               setup { myServoX.attach(X) }
+   *               loop  { myServoX.write(Y)  }
+   * @param {!Blockly.Block} block Blok to generate the code from.
+   * @return {string} Completed code.
+   */
+Blockly.Arduino['connect_smooth_servo_block'] = function(block) {
+  var pinKey = block.getFieldValue('SERVO_PIN');
+  var servoAngle = Blockly.Arduino.valueToCode(
+    block, 'SERVO_ANGLE', Blockly.Arduino.ORDER_ATOMIC) || '90';
+  var servoName = 'myServo' + pinKey;
+  var sweepTime = Blockly.Arduino.valueToCode(
+    block, 'SWEEP_TIME', Blockly.Arduino.ORDER_ATOMIC) || '2';
+
+  Blockly.Arduino.reservePin(
+    block, pinKey, Blockly.Arduino.PinTypes.SERVO, 'Servo Write');
+  
+  Blockly.Arduino.addInclude('servo', '#include <Servo.h>');
+  Blockly.Arduino.addDeclaration('servo_' + pinKey, 'Servo ' + servoName + ';');
+
+  var setupCode = servoName + '.attach(' + pinKey + ');';
+  Blockly.Arduino.addSetup('servo_' + pinKey, setupCode, true);
+
+  var code = servoName + '.write(' + servoAngle + ');\n';
+  code += '// sweep time = ' + sweepTime + '\n';
+  return code;
+}
